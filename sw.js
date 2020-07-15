0f3ebe509eda54fb018828020f6483da8dc9f505
Cwenjie <1915366748@qq.com>
2020-07-15 15:02:22 +0800

    modify

--- sw.js
+++ sw.js
@@ -132,51 +132,7 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "index.html",
     "revision": "47b86dd31d739f2feba5ee526434bfef"
-  },
-  {
-    "url": "love/index.html",
-    "revision": "70f8c4796644f27281feebf0d1873112"
-  },
-  {
-    "url": "love/resource/default.css",
-    "revision": "f734197693fe9b2264472e226160a4ed"
-  },
-  {
-    "url": "love/resource/functions.js",
-    "revision": "0ef9611f34070156005617066f0b46a2"
-  },
-  {
-    "url": "love/resource/jquery.min.js",
-    "revision": "db2cccefedcc741a45a582e91a5afe8d"
-  },
-  {
-    "url": "love/resource/jscex-async-powerpack.min.js",
-    "revision": "fd45d8a1f07587f6c02374252ec473ff"
-  },
-  {
-    "url": "love/resource/jscex-async.min.js",
-    "revision": "aa6f97f754cafa543faaab169e3fd001"
-  },
-  {
-    "url": "love/resource/jscex-builderbase.min.js",
-    "revision": "9b8461afeb0b66c0d0ad1301b41c243c"
-  },
-  {
-    "url": "love/resource/jscex-jit.js",
-    "revision": "c8694188c517a0c89ceb784fbda10e49"
-  },
-  {
-    "url": "love/resource/jscex-parser.js",
-    "revision": "a23cd8fa9a8937aed571c01ba52139a6"
-  },
-  {
-    "url": "love/resource/jscex.min.js",
-    "revision": "c36e85ee92e83d634d0245bc83889d87"
-  },
-  {
-    "url": "love/resource/love.js",
-    "revision": "793cbfa6c1cc3d03d1857e9e557986b1"
-  },
+  }
 ]);
 
 workbox.routing.registerRoute(
